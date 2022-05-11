77f86a859cb99aea7ad1ef54b78dd165af064169
sergiosssg <sergiosssg@gmail.com>
2022-05-11 23:29:46 +0300

    Добавьте файлы проекта.

---
+++ Criterias.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataFileImporter
+{
+    public interface ICriteriaTelefonOperator
+    {
+        public string TelefonOperator
+        {
+            get;
+            set;
+        }
+
+        public string FileName
+        {
+            get;
+            set;
+        }
+
+
+    }
+
+    public interface ICriteriaPhoneNumber
+    {
+        public string PhoneNumber
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataFileImporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
