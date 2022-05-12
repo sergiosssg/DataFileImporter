c1ef812ca2ded267e4d2ef3f03cc03d6afcd1744
sergiosssg <sergiosssg@gmail.com>
2022-05-12 15:36:52 +0300

    Added  class   GlobalCriteriasPool  for  storing  criterias in static  dictionary .

--- Criterias.cs
+++ Criterias.cs
@@ -31,4 +31,18 @@ namespace DataFileImporter
             set;
         }
     }
+
+
+    /**
+     *   This class has dictionary of criteria string
+     * 
+     * 
+     * 
+     */
+    public class GlobalCriteriasPool
+    {
+        public static IDictionary<int, string> DictionaryOfCriteraStrings = new Dictionary<int, string>();
+    }
+
+
 }
