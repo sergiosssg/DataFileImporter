a1cd634f478ad6cea5b8bc045da53335976bd9c3
sergiosssg <sergiosssg@gmail.com>
2022-05-13 09:21:50 +0300

    Created  class  AccountPeriodTime  for  storing  year and month  as integers, and created class  ScannedSeed  for storring  all information  fetched from raw input files of  communication provider companies, such as:     "ukrtelecom", "vodafon",  "kievstar" .

---
+++ AccountPeriodTime.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataFileImporter
+{
+    public class AccountPeriodTime
+    {
+        public int Year
+        {
+            get;
+            set;
+        }
+        public int Month
+        {
+            get;
+            set;
+        }
+
+        public AccountPeriodTime()
+        {
+
+        }
+
+    }
+}
---
+++ ScannedSeed.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataFileImporter
+{
+    public class ScannedSeed
+    {
+        public string TelefonOperator
+        {
+            get;
+            set;
+        }
+        
+    }
+}
