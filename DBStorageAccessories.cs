9b2e90f8fc11e689d6206c6c9a5b6ef09dbe18b0
sergiosssg <sergiosssg@gmail.com>
2022-06-03 15:26:47 +0300

    in class  DataBaseFacilities  added  static  function :    --   public static string GetConnectionString() .

--- DBStorageAccessories.cs
+++ DBStorageAccessories.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,20 @@ namespace DataFileImporter
 {
     public class DataBaseFacilities
     {
+
+        public static string GetConnectionString()
+        {
+            DbConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+            builder["Data Source"] = "localhost";
+            ///builder["Data Source"] = @"localhost\SQLExpress";
+
+            builder["Database"] = "sampd_cexs";
+
+            builder["integrated Security"] = "true";
+
+            return builder.ConnectionString;
+        }
 
     }
 
