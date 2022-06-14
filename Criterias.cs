4aac4062e93ac5b0f5ea8773b22109590ea56c3e
sergiosssg <sergiosssg@gmail.com>
2022-06-14 14:09:48 +0300

    created  class   DataFileImporter .

--- Criterias.cs
+++ Criterias.cs
@@ -7,6 +7,12 @@ using System.Threading.Tasks;
 namespace DataFileImporter
 {
 
+    public partial class DataFileImporter
+    {
+
+    }
+
+
     /**
      *   This class has dictionary of criteria string
      * 
