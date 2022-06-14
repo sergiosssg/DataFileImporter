0f1269d634fe50058ab3d04e20b8879f5942acda
sergiosssg <sergiosssg@gmail.com>
2022-06-14 14:37:11 +0300

    added  component   MainWindow .

---
+++ MainWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataFileImporter
+{
+    public partial class MainWindow : Component
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public MainWindow(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
